4381a6ce95141c39768673e025f2ed190094b218
qq1306162647 <1306162647@qq.com>
2019-07-09 16:35:31 +0800

    web端搭建

---
+++ gyds-core/src/main/java/com/fly/activity/service/impl/ApplyServiceImpl.java
@@ -0,0 +1,57 @@
+package com.fly.activity.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fly.activity.dao.ApplyDao;
+import com.fly.activity.domain.ApplyDO;
+import com.fly.activity.service.ApplyService;
+
+import java.util.List;
+import java.util.Map;
+
+
+
+
+
+@Service
+public class ApplyServiceImpl implements ApplyService {
+	@Autowired
+	private ApplyDao applyDao;
+	
+	@Override
+	public ApplyDO get(Long id){
+		return applyDao.get(id);
+	}
+	
+	@Override
+	public List<ApplyDO> list(Map<String, Object> map){
+		return applyDao.list(map);
+	}
+	
+	@Override
+	public int count(Map<String, Object> map){
+		return applyDao.count(map);
+	}
+	
+	@Override
+	public int save(ApplyDO apply){
+		return applyDao.save(apply);
+	}
+	
+	@Override
+	public int update(ApplyDO apply){
+		return applyDao.update(apply);
+	}
+	
+	@Override
+	public int remove(Long id){
+		return applyDao.remove(id);
+	}
+	
+	@Override
+	public int batchRemove(Long[] ids){
+		return applyDao.batchRemove(ids);
+	}
+	
+}
---
+++ gyds-web/src/main/java/com/fly/system/servicce/impl/RegionServiceImpl.java
@@ -0,0 +1,105 @@
+package com.fly.system.servicce.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.fly.domain.RegionDO;
+import com.fly.domain.Tree;
+import com.fly.system.dao.RegionDao;
+import com.fly.system.servicce.RegionService;
+import com.fly.utils.BuildTree;
+
+
+
+@Service
+public class RegionServiceImpl implements RegionService {
+	@Autowired
+	private RegionDao regionDao;
+	
+	@Override
+	public RegionDO get(Integer regionCode){
+		return regionDao.get(regionCode);
+	}
+	
+	@Override
+	public List<RegionDO> list(Map<String, Object> map){
+		return regionDao.list(map);
+	}
+	
+	@Override
+	public int count(Map<String, Object> map){
+		return regionDao.count(map);
+	}
+	
+	@Override
+	public int save(RegionDO region){
+		return regionDao.save(region);
+	}
+	
+	@Override
+	public int update(RegionDO region){
+		return regionDao.update(region);
+	}
+	
+	@Override
+	public int remove(Integer regionCode){
+		return regionDao.remove(regionCode);
+	}
+	
+	@Override
+	public int batchRemove(Integer[] regionCodes){
+		return regionDao.batchRemove(regionCodes);
+	}
+	
+	@Override
+	public Tree<RegionDO> getTree(Map<String,Object> params) {
+		List<Tree <RegionDO>> trees = new ArrayList<Tree <RegionDO>>();
+		List<RegionDO> RegionDOs = regionDao.list(params);
+		for (RegionDO sysRegion : RegionDOs) {
+			Tree<RegionDO> tree = new Tree <RegionDO>();
+			tree.setId(sysRegion.getRegionCode().toString());
+			tree.setParentId(sysRegion.getParentRegionCode().toString());
+			tree.setText(sysRegion.getRegionName());
+			Map<String, Object> state = new HashMap<>(16);
+			state.put("opened", false);
+			tree.setState(state);
+			trees.add(tree);
+		}
+		// 默认顶级菜单为０，根据数据库实际情况调整
+		Tree <RegionDO> t = BuildTree.build(trees,-1);
+		return t;
+	}
+	
+	@Override
+	public Tree<RegionDO> getRegionTree(Map<String,Object> params) {
+		List<Tree <RegionDO>> trees = new ArrayList<Tree <RegionDO>>();
+		List<RegionDO> RegionDOs = regionDao.regionIdByList(params);
+		RegionDO regionDO = regionDao.get(Integer.parseInt(params.get("pids").toString()));
+		RegionDOs.add(regionDO);
+		for (RegionDO sysRegion : RegionDOs) {
+			Tree<RegionDO> tree = new Tree <RegionDO>();
+			tree.setId(sysRegion.getRegionCode().toString());
+			tree.setParentId(sysRegion.getParentRegionCode().toString());
+			tree.setText(sysRegion.getRegionName());
+			Map<String, Object> state = new HashMap<>(16);
+			state.put("opened", false);
+			tree.setState(state);
+			trees.add(tree);
+		}
+		// 默认顶级菜单为０，根据数据库实际情况调整
+		Tree <RegionDO> t = BuildTree.build(trees,regionDO.getParentRegionCode());
+		return t;
+	}
+
+	@Override
+	public boolean checkRegionHasUser(Integer regionCode) {
+		int result = regionDao.getRegionUserNumber(regionCode);
+		return result==0?true:false;
+	}
+
+	
+}
