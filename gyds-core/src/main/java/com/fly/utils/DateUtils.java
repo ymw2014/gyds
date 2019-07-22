adf3c5cebf2fde5e84ec0da210733861d1c65728
qq1306162647 <1306162647@qq.com>
2019-07-22 18:46:46 +0800

    代码提交

--- gyds-core/src/main/java/com/fly/utils/DateUtils.java
+++ gyds-core/src/main/java/com/fly/utils/DateUtils.java
@@ -24,99 +24,99 @@ public class DateUtils {
 	public final static String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
 
-    public static String format(Date date) {
-        return format(date, DATE_PATTERN);
-    }
-    public static Date parse(String date) {
-        return parse(date, DATE_PATTERN);
-    }
-    
-    public static String format(Date date, String pattern) {
-        if (date != null) {
-            SimpleDateFormat df = new SimpleDateFormat(pattern);
-            return df.format(date);
-        }
-        return null;
-    }
-    public static Date parse(String date, String pattern) {
-        if (date != null&&date!="") {
-            SimpleDateFormat df = new SimpleDateFormat(pattern);
-            try {
+	public static String format(Date date) {
+		return format(date, DATE_PATTERN);
+	}
+	public static Date parse(String date) {
+		return parse(date, DATE_PATTERN);
+	}
+
+	public static String format(Date date, String pattern) {
+		if (date != null) {
+			SimpleDateFormat df = new SimpleDateFormat(pattern);
+			return df.format(date);
+		}
+		return null;
+	}
+	public static Date parse(String date, String pattern) {
+		if (date != null&&date!="") {
+			SimpleDateFormat df = new SimpleDateFormat(pattern);
+			try {
 				return df.parse(date);
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
-        }
-        return null;
-    }
-    /**
-     * 计算距离现在多久，非精确
-     *
-     * @param date
-     * @return
-     */
-    public static String getTimeBefore(Date date) {
-        Date now = new Date();
-        long l = now.getTime() - date.getTime();
-        long day = l / (24 * 60 * 60 * 1000);
-        long hour = (l / (60 * 60 * 1000) - day * 24);
-        long min = ((l / (60 * 1000)) - day * 24 * 60 - hour * 60);
-        long s = (l / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
-        String r = "";
-        if (day > 0) {
-            r += day + "天";
-        } else if (hour > 0) {
-            r += hour + "小时";
-        } else if (min > 0) {
-            r += min + "分";
-        } else if (s > 0) {
-            r += s + "秒";
-        }
-        r += "前";
-        return r;
-    }
+		}
+		return null;
+	}
+	/**
+	 * 计算距离现在多久，非精确
+	 *
+	 * @param date
+	 * @return
+	 */
+	public static String getTimeBefore(Date date) {
+		Date now = new Date();
+		long l = now.getTime() - date.getTime();
+		long day = l / (24 * 60 * 60 * 1000);
+		long hour = (l / (60 * 60 * 1000) - day * 24);
+		long min = ((l / (60 * 1000)) - day * 24 * 60 - hour * 60);
+		long s = (l / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+		String r = "";
+		if (day > 0) {
+			r += day + "天";
+		} else if (hour > 0) {
+			r += hour + "小时";
+		} else if (min > 0) {
+			r += min + "分";
+		} else if (s > 0) {
+			r += s + "秒";
+		}
+		r += "前";
+		return r;
+	}
 
-    /**
-     * 计算距离现在多久，精确
-     *
-     * @param date
-     * @return
-     */
-    public static String getTimeBeforeAccurate(Date date) {
-        Date now = new Date();
-        long l = now.getTime() - date.getTime();
-        long day = l / (24 * 60 * 60 * 1000);
-        long hour = (l / (60 * 60 * 1000) - day * 24);
-        long min = ((l / (60 * 1000)) - day * 24 * 60 - hour * 60);
-        long s = (l / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
-        String r = "";
-        if (day > 0) {
-            r += day + "天";
-        }
-        if (hour > 0) {
-            r += hour + "小时";
-        }
-        if (min > 0) {
-            r += min + "分";
-        }
-        if (s > 0) {
-            r += s + "秒";
-        }
-        r += "前";
-        return r;
-    }
-    /*
-     * 计算开始结束相差时间
-     */
-    public static int  longOfTwoDate(Date first,Date second) throws ParseException{
-    	                 Calendar calendar = Calendar.getInstance();
-    	                 calendar.setTime(first);
-    	                 int cnt = 0;
-    	                 while(calendar.getTime().compareTo(second)!=0){
-    	                     calendar.add(Calendar.DATE, 1);
-    	                    cnt++;
-    	                 }
-    	                 return cnt;
-    	     }
+	/**
+	 * 计算距离现在多久，精确
+	 *
+	 * @param date
+	 * @return
+	 */
+	public static String getTimeBeforeAccurate(Date date) {
+		Date now = new Date();
+		long l = now.getTime() - date.getTime();
+		long day = l / (24 * 60 * 60 * 1000);
+		long hour = (l / (60 * 60 * 1000) - day * 24);
+		long min = ((l / (60 * 1000)) - day * 24 * 60 - hour * 60);
+		long s = (l / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+		String r = "";
+		if (day > 0) {
+			r += day + "天";
+		}
+		if (hour > 0) {
+			r += hour + "小时";
+		}
+		if (min > 0) {
+			r += min + "分";
+		}
+		if (s > 0) {
+			r += s + "秒";
+		}
+		r += "前";
+		return r;
+	}
+	/*
+	 * 计算开始结束相差时间
+	 */
+	public static int  longOfTwoDate(Date first,Date second) throws ParseException{
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(first);
+		int cnt = 0;
+		while(calendar.getTime().compareTo(second)!=0){
+			calendar.add(Calendar.DATE, 1);
+			cnt++;
+		}
+		return cnt;
+	}
 
 }
