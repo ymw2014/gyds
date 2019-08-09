68d0e0c26b53d46c9a619ccf05feb13479a941f2
wangyanke <443666413@qq.com>
2019-08-09 17:03:38 +0800

    2019080917

--- gyds-manager/src/main/java/com/fly/team/controller/NameController.java
+++ gyds-manager/src/main/java/com/fly/team/controller/NameController.java
@@ -1,11 +1,14 @@
 package com.fly.team.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fly.domain.RegionDO;
+import com.fly.system.service.RegionService;
+import com.fly.team.domain.TeamDO;
+import com.fly.team.domain.TypeDO;
+import com.fly.team.service.TeamService;
+import com.fly.team.service.TeamTypeService;
+import com.fly.utils.BeanUtil;
+import com.fly.utils.Dictionary;
+import com.fly.utils.JSONUtils;
 import com.fly.utils.PageUtils;
 import com.fly.utils.Query;
 import com.fly.utils.R;
@@ -33,6 +45,12 @@ import com.fly.verifyName.service.NameService;
 public class NameController {
 	@Autowired
 	private NameService nameService;
+	@Autowired
+	private TeamTypeService teamTypeService;
+	@Autowired
+	private TeamService teamService;
+	@Autowired
+	private RegionService regionService;
 	
 	@GetMapping()
 	@RequiresPermissions("verifyName:name:name")
@@ -45,10 +63,20 @@ public class NameController {
 	@RequiresPermissions("verifyName:name:name")
 	public PageUtils list(@RequestParam Map<String, Object> params){
 		//查询列表数据
+		params.put("type", Dictionary.JIAN_TUAN_SHEN_QING);
         Query query = new Query(params);
 		List<NameDO> nameList = nameService.list(query);
+		List<Map<String,Object>> list=new ArrayList<Map<String,Object>>();
+		for (NameDO nameDO : nameList) {
+			Map<String, Object> teamMap = JSONUtils.jsonToMap(nameDO.getText());
+			Map<String, Object> map = BeanUtil.transBean2Map(nameDO);
+			map.putAll(teamMap);
+			TypeDO type = teamTypeService.get(Integer.parseInt(teamMap.get("teamType").toString()));
+			map.put("teamType", type.getTypeName());
+			list.add(map);
+		}
 		int total = nameService.count(query);
-		PageUtils pageUtils = new PageUtils(nameList, total);
+		PageUtils pageUtils = new PageUtils(list, total);
 		return pageUtils;
 	}
 	
@@ -111,6 +139,57 @@ public class NameController {
 	public R remove(@RequestParam("ids[]") Integer[] ids){
 		nameService.batchRemove(ids);
 		return R.ok();
+	}
+	
+	/**
+	 *	建团信息审核
+	 * @param id
+	 * @param status
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("/examine")
+	@RequiresPermissions("member:member:shenhe")
+	@Transactional
+	public R examine(Integer id,Integer status) {
+		NameDO name = nameService.get(id);
+		name.setStatus(status);
+		if(status==1) {//审核通过,保存团队信息
+			name.getText();
+			TeamDO team=(TeamDO)JSONUtils.jsonToBean(name.getText(), new TeamDO());
+			team.setId(randomCode(team.getRegCode()));
+			teamService.save(team);
+			RegionDO region=new RegionDO();
+			region.setRegionCode(team.getId());
+			region.setParentRegionCode(team.getRegCode());
+			region.setRegionName(team.getTeamName());
+			region.setRegionType(2);
+			region.setRegionLevel(5);
+			regionService.save(region);
+		}
+		if(nameService.update(name)>0){
+			return R.ok();
+		}
+		return R.error();
+	}
+	
+	/**
+	 * 生成团队编号
+	 * @param regCode
+	 * @return
+	 */
+	public Integer randomCode(Integer regCode) {
+		Integer random =(int) (Math.random()*(999-100+1)+100);
+		String strCode = regCode.toString().substring(2, regCode.toString().length());
+		strCode = strCode+random;
+		Integer code = Integer.valueOf(strCode);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("id",code);
+		List<TeamDO> list = teamService.list(map);
+		if(list.size()>0) {
+			randomCode(regCode);
+		}
+		return code;
 	}
 	
 }
