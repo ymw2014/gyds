8955a31f269b3f20ac043e4d4a29100f5c5bb68f
sxtbeihai <15239233375@163.com>
2019-08-21 11:39:48 +0800

    appwxpay

--- gyds-manager/src/main/java/com/fly/wxpay/controller/WxPayController.java
+++ gyds-manager/src/main/java/com/fly/wxpay/controller/WxPayController.java
@@ -1,5 +1,9 @@
 package com.fly.wxpay.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.InterfaceAddress;
@@ -23,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.fly.wxpay.service.IWxPayConfig;
 import com.github.binarywang.utils.qrcode.MatrixToImageWriter;
 import com.github.wxpay.sdk.WXPay;
+import com.github.wxpay.sdk.WXPayUtil;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
@@ -35,21 +40,15 @@ public class WxPayController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	/*
-	 * @Value("${wxpay.appid}") private String app_id;
-	 * 
-	 * @Value("${wxpay.key}") private String wx_pay_key;
-	 * 
-	 * @Value("${wxpay.mch_id}") private String wx_pay_mch_id;
-	 */
+	
 
 	@RequestMapping("/pay")
 	public String payWeChat(String ids, String content, HttpServletResponse response, HttpServletRequest request,
-			ModelMap model) {
+			ModelMap model, String totalFee) {
 		logger.info("进入支付方法，payWeChat");
 		try {
-			content = this.getImageUrl("余额充值", "1", "1", "");
-			logger.info("wxpay codeUrl，content" + content);
+			content = this.getImageUrl("余额充值", "6578754569698556", totalFee);
+			logger.info("wxpay code_url: " + content);
 			MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
 			Hashtable hints = new Hashtable();
 			hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
@@ -57,45 +56,105 @@ public class WxPayController {
 			response.setContentType("image/jpeg");
 			MatrixToImageWriter.writeToStream(bitMatrix, "jpg", response.getOutputStream());
 		} catch (Exception e) {
-			e.printStackTrace();
-			logger.warn("订单已支付，请勿重新支付");
+			logger.error(e.getMessage());
+			logger.warn("支付失败");
 		}
 		return null;
 	}
+	
+	@RequestMapping("/callback")
+	public String callBack(HttpServletRequest request, HttpServletResponse response,String xmlData) throws Exception {
+		logger.info("进入微信支付异步通知");
+		logger.info("callback() start, notixmlData={}", xmlData);
+	    String resXml="";
+	    try{
+	        //
+	        InputStream is = request.getInputStream();
+	        //将InputStream转换成String
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+	        StringBuilder sb = new StringBuilder();
+	        String line = null;
+	        try {
+	            while ((line = reader.readLine()) != null) {
+	                sb.append(line + "\n");
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	            try {
+	                is.close();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	        resXml=sb.toString();
+	        logger.info("微信支付异步通知请求包: {}", resXml);
+	        return payBack(resXml);
+	    }catch (Exception e){
+	        logger.error("微信支付回调通知失败",e);
+	        String result = "-1";
+	        return result;
+	    }
+	}
+	
+	public String payBack(String notifyData) throws Exception {
+	    logger.info("payBack() start, notifyData={}", notifyData);
+	    IWxPayConfig config = new IWxPayConfig();
+		WXPay wxpay = new WXPay(config);
+	    String xmlBack="";
+	    Map<String, String> notifyMap = null;
+	    try {
 
-	public String getImageUrl(String body, String out_trade_no, String product_id, String total_fee) throws Exception {
+	        notifyMap = WXPayUtil.xmlToMap(notifyData);         // 转换成map
+	        if (wxpay.isPayResultNotifySignatureValid(notifyMap)) {
+	            // 签名正确
+	            // 进行处理。
+	            // 注意特殊情况：订单已经退款，但收到了支付结果成功的通知，不应把商户侧订单状态从退款改成支付成功
+	            String return_code = notifyMap.get("return_code");//状态
+	            String out_trade_no = notifyMap.get("out_trade_no");//订单号
+
+	            if (out_trade_no == null) {
+	                logger.info("微信支付回调失败订单号: {}", notifyMap);
+	                xmlBack = "-1";
+	                return xmlBack;
+	            }
+
+	            // 业务逻辑处理 ****************************
+	            logger.info("微信支付回调成功订单号: {}", notifyMap);
+	            xmlBack = "1";
+	            return xmlBack;
+	        } else {
+	            logger.error("微信支付回调通知签名错误");
+	            xmlBack = "-1";
+	            return xmlBack;
+	        }
+	    } catch (Exception e) {
+	        logger.error("微信支付回调通知失败",e);
+	        xmlBack = "-1";
+	    }
+	    return xmlBack;
+	}
+
+	public String getImageUrl(String body, String out_trade_no, String total_fee) throws Exception {
 		Map<String, String> paramMap = new HashMap<String, String>();
 		IWxPayConfig config = new IWxPayConfig();
 		WXPay wxpay = new WXPay(config);
 		String code_url = "";
-		// paramMap.put("appid", "wx561ae40290380b04"); //appid：每个公众号都有一个appid
 		paramMap.put("body", body); // 商品描述
-		// paramMap.put("mch_id", "1309497501"); //商户号：开通微信支付后分配
-		// paramMap.put("nonce_str", WXPayUtil.generateNonceStr()); // 随机字符串
 		paramMap.put("notify_url", "http://yqhdb.com/payment/wechat/pay.php"); // 支付成功后，回调地址
 		paramMap.put("out_trade_no", out_trade_no); // 商户订单号
-		// paramMap.put("product_id", product_id); // 商户根据自己业务传递的参数
-		// 当trade_type=NATIVE时必填
 		paramMap.put("spbill_create_ip", this.localIp()); // 本机的Ip
-		paramMap.put("total_fee", "1"); // 金额必须为整数 单位为分
+		paramMap.put("total_fee", total_fee); // 金额必须为整数 单位为分
 		paramMap.put("trade_type", "NATIVE"); // 交易类型
-		// paramMap.put("sign", WXPayUtil.generateSignature(paramMap,
-		// "fenlegou20160226fenlegou20160226"));//根据微信签名规则，生成签名。随机参数可以在商户后台管理系统中进行设置。
 
 		try {
 			Map<String, String> resp = wxpay.unifiedOrder(paramMap);
 			System.out.println(resp);
 			code_url = resp.get("code_url");
-			// return MapHelper.success("code_url", resp.get("code_url"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		// return MapHelper.error();
 
-		/*
-		 * String xmlData = WXPayUtil.mapToXml(paramMap);//把参数转换成XML数据格式 String codeUrl
-		 * = getCodeUrl(xmlData); //获取二维码链接
-		 */
 		return code_url;
 	}
 
@@ -120,26 +179,5 @@ public class WxPayController {
 		return ip;
 	}
 
-	/**
-	 * 获取二维码链接
-	 * 
-	 * @param xmlData
-	 * @return
-	 *//*
-		 * private String getCodeUrl(String xmlData) { String resXml = null; String
-		 * code_url = ""; Map<String, String> map; try {
-		 * HttpUtils.postData("https://api.mch.weixin.qq.com/pay/unifiedorder",
-		 * xmlData); map = WXPayUtil.xmlToMap(resXml); Object returnCode =
-		 * map.get("return_code"); if("SUCCESS".equals(returnCode)) { Object resultCode
-		 * = map.get("result_code"); if("SUCCESS".equals(resultCode)) { code_url =
-		 * map.get("code_url").toString(); } } } catch (UnrecoverableKeyException e1) {
-		 * logger.error(e1.getMessage()); } catch (KeyManagementException e1) {
-		 * logger.error(e1.getMessage()); } catch (KeyStoreException e1) {
-		 * logger.error(e1.getMessage()); } catch (NoSuchAlgorithmException e1) {
-		 * logger.error(e1.getMessage()); } catch (IOException e1) {
-		 * logger.error(e1.getMessage()); }catch (Exception e) {
-		 * logger.error(e.getMessage()); return ""; }
-		 * 
-		 * return code_url; }
-		 */
+	
 }
